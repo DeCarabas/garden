f744730f940e8c116139442244733a8b783867c6
John Doty <john@d0ty.me>
2018-12-27 08:07:02 -0800

    Content, kinda

--- xna/game/GardenGame.cs
+++ xna/game/GardenGame.cs
@@ -1,20 +1,38 @@
 using System;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace Garden
 {
     class GardenGame : Game
     {
         GraphicsDeviceManager graphics;
+        SpriteFont font;
 
         public GardenGame()
         {
             graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "content";
+        }
+
+        protected override void LoadContent()
+        {
+            font = Content.Load<SpriteFont>("fonts/basic");
+        }
+
+        protected override void UnloadContent()
+        {
+            font = null;
         }
 
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            var spriteBatch = new SpriteBatch(GraphicsDevice);
+            spriteBatch.Begin();
+            spriteBatch.DrawString(font, "Score", new Vector2(100, 100), Color.Black);
+            spriteBatch.End();
         }
     }
 }
