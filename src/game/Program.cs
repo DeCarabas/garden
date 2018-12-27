7a151e015cdb7fd1644c9f42bba76a7a53142a1e
John Doty <john@d0ty.me>
2018-12-27 06:36:14 -0800

    Starting with C#

---
+++ src/game/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace Garden
+{
+    class GardenGame : Game
+    {
+        GraphicsDeviceManager graphics;
+
+        public GardenGame()
+        {
+            graphics = new GraphicsDeviceManager(this);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                new GardenGame().Run();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("SAD: {0}", e);
+            }
+        }
+    }
+}
